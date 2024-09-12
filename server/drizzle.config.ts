a767a8ff3dee1ebc8df1ed61bb7e240eaefb4a68
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-09-12 22:25:42 +0100

    .env and biome, drizzle config

---
+++ server/drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	schema: "./schema.ts",
+  out: "./.migrations",
+	dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://docker:docker@localhost:5432/inorbit"
+  },
+});
