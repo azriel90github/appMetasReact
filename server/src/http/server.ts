2e8395b32b8ebc81893ea95843a5873b5b6bb2a2
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-09-12 13:41:44 +0100

    docker config

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,9 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
-const app = fastify()
+const app = fastify();
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server  running!')
-})
+app
+	.listen({
+		port: 3333,
+	})
+	.then(() => {
+		console.log("HTTP server  running!");
+	});
