5b6330104f420a07244790872f30fd248f5b7251
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-09-14 07:57:15 +0100

    migration and postgres install

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,8 +1,10 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
 
 export const goals = pgTable("goals", {
-	id: text('id').primaryKey(),
-  title: text('title').notNull(),
-  desireWeeklyFrequence: integer('desired_wekly_frequence').notNull(),
-  createAt: timestamp('create_at', { withTimezone: true }).notNull()
+	id: text("id").primaryKey(),
+	title: text("title").notNull(),
+	desireWeeklyFrequence: integer("desired_wekly_frequence").notNull(),
+	createAt: timestamp("create_at", { withTimezone: true })
+  .notNull()
+  .defaultNow(),
 });
