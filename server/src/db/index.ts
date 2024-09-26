0e50edd7c4c709a6972a8f052672622dc8673693
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-09-26 16:12:56 +0100

    seed update

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,5 +3,6 @@ import postgres from "postgres";
 import * as schema from './schema';
 import { env } from "../env";
 
+
 export const client = postgres(env.DATABASE_URL)
 export const db = drizzle(client, { schema, logger: true})
--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -6,9 +6,9 @@ async function seed() {
   await db.delete(goals)
 
   await db.insert(goals).values([
-    { title: 'Acordar cedo', desireWeeklyFrequence: 5 }
-    { title: 'Me exercitar', desireWeeklyFrequence: 3 }
-    { title: 'Meditar', desireWeeklyFrequence: 1 }
+    { title: 'Acordar cedo', desireWeeklyFrequence: 5 },
+    { title: 'Me exercitar', desireWeeklyFrequence: 3 },
+    { title: 'Meditar', desireWeeklyFrequence: 1 },
   ])
 }
 
