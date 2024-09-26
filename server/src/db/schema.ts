c21e514712e1b35dc9bbaa7aac562e177c029cca
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-09-26 14:16:07 +0100

    parallel add

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,7 +1,8 @@
 import { pgTable, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { createId } from '@paralleldrive/cuid2'
 
 export const goals = pgTable("goals", {
-	id: text("id").primaryKey(),
+	id: text("id").primaryKey().$defaultFn(() => createId()),
 	title: text("title").notNull(),
 	desireWeeklyFrequence: integer("desired_wekly_frequence").notNull(),
 	createAt: timestamp("create_at", { withTimezone: true })
