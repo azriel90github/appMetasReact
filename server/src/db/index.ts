26d385481e9bfd0c2ca9df38ebf63ceb802ef1fe
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2024-09-30 12:32:31 +0100

    index update

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,8 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from './schema';
+import * as schema from "./schema";
 import { env } from "../env";
 
-
-export const client = postgres(env.DATABASE_URL)
-export const db = drizzle(client, { schema, logger: true })
+export const client = postgres(env.DATABASE_URL);
+export const db = drizzle(client, { schema, logger: true }); 
